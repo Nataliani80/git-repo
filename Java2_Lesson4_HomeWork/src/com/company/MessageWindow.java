3b85f2336373e4bf55b2e8e97b50d1847c6ae8da
Nataliani80 <As-Terpsixora-312@yandex.ru>
2019-03-14 20:53:40 +0300

    Отредактировала ДЗ JAVA2 Lesson 4

--- Java2_Lesson4_HomeWork/src/com/company/MessageWindow.java
+++ Java2_Lesson4_HomeWork/src/com/company/MessageWindow.java
@@ -11,9 +11,8 @@ public class MessageWindow extends JFrame {
         setTitle("Messenger");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(300, 300, 400, 400);
-        
+
         JPanel correspondenceField = new JPanel();
-        correspondenceField.setBackground(Color.LIGHT_GRAY);
         correspondenceField.setLayout(new BoxLayout(correspondenceField, BoxLayout.Y_AXIS));
         add(correspondenceField, BorderLayout.CENTER);
 
@@ -26,7 +25,7 @@ public class MessageWindow extends JFrame {
         JPanel inputMessageContainer = new JPanel();
         inputMessageContainer.setBackground(Color.WHITE);
         add(inputMessageContainer, BorderLayout.PAGE_END);
-        inputMessageContainer.setPreferredSize(new Dimension(400,60));
+        inputMessageContainer.setPreferredSize(new Dimension(400,35));
         inputMessageContainer.setLayout(new BoxLayout(inputMessageContainer, BoxLayout.X_AXIS));
 
         JTextField inputMessage = new JTextField();
@@ -38,7 +37,7 @@ public class MessageWindow extends JFrame {
         inputMessage.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                messages.append(inputMessage.getText() + "\n\r");
+                messages.append(inputMessage.getText() + "\n");
                 inputMessage.setText(null);
             }
         });
@@ -46,7 +45,7 @@ public class MessageWindow extends JFrame {
         sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                messages.append(inputMessage.getText() + "\n\r");
+                messages.append(inputMessage.getText() + "\n");
                 inputMessage.setText(null);
             }
         });
